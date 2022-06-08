fec22b130ecca89639dc0ba7f2a0600ddff40106
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-06-09 00:17:28 +0200

    update main to use created messages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,14 +11,11 @@ fun main() {
     }
     runBlocking {
         client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-            while(true) {
-                val othersMessage = incoming.receive() as? Frame.Text ?: continue
-                println(othersMessage.readText())
-                val myMessage = readLine()
-                if(myMessage != null) {
-                    send(myMessage)
-                }
-            }
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
+
+            userInputRoutine.join() // Wait for completion; either "exit" or error
+            messageOutputRoutine.cancelAndJoin()
         }
     }
     client.close()
